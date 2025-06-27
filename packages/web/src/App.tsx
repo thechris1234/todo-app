43c14c28a92842fc7b75bfe557546be58afb9430
thechris1234 <ludvi81@gmail.com>
2025-06-27 12:43:30 +0200

    Initial commit

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <div>asd</div>
+            <h1 className="text-3xl font-bold underline">Hell o world!</h1>
+        </>
+    );
+}
+
+export default App;
